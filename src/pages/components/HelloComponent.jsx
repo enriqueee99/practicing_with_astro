50df46b50a4ca2552fc1e14635a9cd648a5e37ff
Anthony Enrique <enriqueee99@gmail.com>
2025-11-08 19:24:17 -0500

    agregue componentes

---
+++ src/pages/components/HelloComponent.jsx
@@ -0,0 +1,10 @@
+function HelloComponent() {
+  return (
+    <div>
+      <h1>Hola componente</h1>
+      <button onClick={() => alert("funcionando")}>enviar</button>
+    </div>
+  );
+}
+
+export default HelloComponent;
